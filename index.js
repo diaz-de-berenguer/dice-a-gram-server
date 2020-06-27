14f80a1e82dab5336d1e70736d5127307292f05f
Diego Diaz <d.diazdeberenguer@gmail.com>
2020-06-27 01:44:39 -0400

    Update server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const webSocketsServerPort = 8000;
-const webSocketServer = require("websocket").server;
+// const webSocketsServerPort = 8000;
+// const webSocketServer = require("websocket").server;
 const http = require("http");
-// Spinning the http server and the websocket server.
-const server = http.createServer();
-server.listen(webSocketsServerPort);
-const wsServer = new webSocketServer({
-  httpServer: server,
-});
+// // Spinning the http server and the websocket server.
+// const server = http.createServer();
+// server.listen(webSocketsServerPort);
+// const wsServer = new webSocketServer({
+//   httpServer: server,
+// });
 
-// I'm maintaining all active connections in this object
-const clients = {};
+const express = require("express");
+const { Server } = require("ws");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+const server = http.createServer(app);
+
+const wss = new Server({ server });
+
+// const clients = {};
 const rolls = {
   1: 0,
   2: 0,
@@ -25,50 +34,57 @@ const rolls = {
   12: 0,
 };
 
-// This code generates unique userid for everyuser.
-const getUniqueID = () => {
-  const s4 = () =>
-    Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  return s4() + s4() + "-" + s4();
-};
+// // This code generates unique userid for everyuser.
+// const getUniqueID = () => {
+//   const s4 = () =>
+//     Math.floor((1 + Math.random()) * 0x10000)
+//       .toString(16)
+//       .substring(1);
+//   return s4() + s4() + "-" + s4();
+// };
 
-wsServer.on("request", function (request) {
-  const userID = getUniqueID();
+wss.on("connection", function (connection) {
+  // const userID = getUniqueID();
   console.log(
     new Date() +
       " Recieved a new connection from origin " +
-      request.origin +
+      Object.keys(connection) +
       "."
   );
-  // You can rewrite this part of the code to accept only the requests from allowed origin
-  const connection = request.accept(null, request.origin);
-  connection.sendUTF(JSON.stringify({ type: "roll", rolls }));
-  clients[userID] = connection;
-  console.log(
-    "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
-  );
+  connection.send(JSON.stringify({ type: "roll", rolls }));
+  // // You can rewrite this part of the code to accept only the requests from allowed origin
+  // const connection = request.accept(null, request.origin);
+  // connection.sendUTF(JSON.stringify({ type: "roll", rolls }));
+  // clients[userID] = connection;
+  // console.log(
+  //   "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
+  // );
 
   connection.on("message", (message) => {
     console.log("gotta message", message);
-    const _message = JSON.parse(message.utf8Data);
+    const _message = JSON.parse(message);
     if (_message.type === "roll") {
       const { number } = _message;
       const currentValue = rolls[number];
       rolls[number] = currentValue + 1;
-      Object.values(clients).forEach((clientConnection) => {
-        clientConnection.sendUTF(JSON.stringify({ type: "roll", rolls }));
-      });
+      // Object.values(clients).forEach((clientConnection) => {
+      //   clientConnection.sendUTF(JSON.stringify({ type: "roll", rolls }));
+      // });
+      // wss.clients.forEach((client) => {
+      //   client.send(JSON.stringify({ type: "roll", rolls }));
+      // });
     }
     if (_message.type === "reset") {
       Object.keys(rolls).forEach((number) => {
         rolls[number] = 0;
       });
-      Object.values(clients).forEach((clientConnection) => {
-        clientConnection.sendUTF(JSON.stringify({ type: "roll", rolls }));
-      });
+      // Object.values(clients).forEach((clientConnection) => {
+      //   clientConnection.sendUTF(JSON.stringify({ type: "roll", rolls }));
+      // });
     }
+    wss.clients.forEach((client) => {
+      client.send(JSON.stringify({ type: "roll", rolls }));
+    });
   });
 
   connection.on("close", function (reasonCode, description) {
@@ -78,3 +94,7 @@ wsServer.on("request", function (request) {
     });
   });
 });
+
+server
+  // .use((req, res) => res.sendFile())
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
